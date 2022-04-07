fe04531de0bef91060074fbd27a8f8ffaa92aa51
yeoni0128 <102969981+yeoni0128@users.noreply.github.com>
2022-04-07 06:07:16 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+
+  background(255, 204, 0);
+  if(keyIsDown(RIGHT_ARROW)){
+    background('red')
+  }
+  rect(190,190,20,20)
+
+}
+
+
+
+
